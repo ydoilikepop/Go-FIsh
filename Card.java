2e06831e3fe0416637006b2782dc0d0df351da40
ydoilikepop <Jason_Z.hen@hotmail.com>
2017-05-11 00:52:33 -0400

    added card

---
+++ Card.java
@@ -0,0 +1,13 @@
+
+public class Card {
+	
+	// Member variables 
+	int value;
+	char suit; 
+	
+	//Constructor 
+	public Card (int value, char suit) {
+		this.value = value;
+		this.suit = suit; 
+	}
+}
